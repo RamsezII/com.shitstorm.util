debd4511ea3c4866d977aa5cba486036e312429d
ram16 <thomass.montaine@gmail.com>
2025-06-21 22:25:37 +0200

    STACKTRACE !

---
+++ Runtime/Errors/Util_err.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+partial class Util
+{
+    public static string GetStackTrace_old()
+    {
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception e)
+        {
+            return e.StackTrace;
+        }
+    }
+
+    public static StackTrace GetStackTrace(in int skipFrames = 1)
+    {
+        // skipFrames = 1 pour ne pas inclure cet appel à GetStackTrace dans la stack
+        return new StackTrace(skipFrames, true);
+    }
+}
