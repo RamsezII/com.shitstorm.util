18eef23ae1342ecb60b7e7bef57f189737e9c9db
ram16 <thomass.montaine@gmail.com>
2025-06-22 02:54:23 +0200

    glob to regex

---
+++ Runtime/Strings/_patterns.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+partial class Util
+{
+    public static string GlobToRegex(this string pattern)
+    {
+        var regex = Regex.Escape(pattern)
+            .Replace(@"\*", ".*")
+            .Replace(@"\?", ".");
+        return "^" + regex + "$";
+    }
+}
