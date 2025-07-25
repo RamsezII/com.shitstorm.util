ad7938eb4b588ff47978aae89383f90490272c0c
ram16 <thomass.montaine@gmail.com>
2025-07-25 20:47:39 +0200

    ui primitive shapes

---
+++ Runtime/UI/UIDiskRenderer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _UTIL_
+{
+    public sealed class UIDiskRenderer : Graphic
+    {
+        public float radius = 50f;
+        public int segments = 16;
+
+        //--------------------------------------------------------------------------------------------------------------
+        [ContextMenu(nameof(SetVerticesDirty))]
+        public override void SetVerticesDirty() => base.SetVerticesDirty();
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+
+            UIVertex vertex = UIVertex.simpleVert;
+            vertex.color = color;
+            float angleStep = 360f / segments;
+
+            vh.AddVert(vertex); // Center vertex
+
+            for (int i = 0; i <= segments; i++)
+                if (i == segments)
+                    vh.AddTriangle(0, 1, i); // Connect last segment to the first
+                else
+                {
+                    float angle = i * angleStep * Mathf.Deg2Rad;
+                    vertex.position = new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius);
+
+                    vh.AddVert(vertex);
+
+                    vh.AddTriangle(0, i + 1, i);
+                }
+        }
+    }
+}
---
+++ Runtime/UI/UIOutlineRenderer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _UTIL_
+{
+    public sealed class UIOutlineRenderer : Graphic
+    {
+        public float tickness = 10f;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        [ContextMenu(nameof(SetVerticesDirty))]
+        public override void SetVerticesDirty() => base.SetVerticesDirty();
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            //base.OnPopulateMesh(vh);
+
+            vh.Clear();
+
+            Rect r = rectTransform.rect;
+            UIVertex vertex = UIVertex.simpleVert;
+            vertex.color = color;
+
+            // out
+            vertex.position = new(0, 0);
+            vh.AddVert(vertex);
+            vertex.position = new(r.width, 0);
+            vh.AddVert(vertex);
+            vertex.position = new(r.width, r.height);
+            vh.AddVert(vertex);
+            vertex.position = new(0, r.height);
+            vh.AddVert(vertex);
+
+            // in
+            vertex.position = new(tickness, tickness);
+            vh.AddVert(vertex);
+            vertex.position = new(r.width - tickness, tickness);
+            vh.AddVert(vertex);
+            vertex.position = new(r.width - tickness, r.height - tickness);
+            vh.AddVert(vertex);
+            vertex.position = new(tickness, r.height - tickness);
+            vh.AddVert(vertex);
+
+            vh.AddTriangle(0, 4, 5);
+            vh.AddTriangle(0, 5, 1);
+            vh.AddTriangle(1, 5, 6);
+            vh.AddTriangle(1, 6, 2);
+            vh.AddTriangle(6, 2, 7);
+            vh.AddTriangle(2, 7, 3);
+            vh.AddTriangle(3, 7, 4);
+            vh.AddTriangle(3, 4, 0);
+        }
+    }
+}
