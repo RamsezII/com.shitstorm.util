496db830f877b1fae670d344da92836a82a17672
ram16 <thomass.montaine@gmail.com>
2025-09-03 22:39:28 +0200

    navmeshes

---
+++ Runtime/AI/Util_ai.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
+
+partial class Util
+{
+    public static IEnumerator<float> EBuildNavMesh(this NavMeshSurface surf)
+    {
+        NavMeshData data = surf.navMeshData;
+
+        if (data == null)
+        {
+            data = new(surf.agentTypeID)
+            {
+                name = $"RuntimeNavMesh_{surf.name}"
+            };
+            surf.navMeshData = data;
+            surf.AddData();
+        }
+
+        AsyncOperation op = surf.UpdateNavMesh(data);
+
+        while (!op.isDone)
+            yield return op.progress;
+    }
+}
