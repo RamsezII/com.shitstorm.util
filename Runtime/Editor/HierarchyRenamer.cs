66635561df15a95a8567e4ec6c403ca60685f559
ram16 <thomass.montaine@gmail.com>
2025-09-05 15:31:12 +0200

    nice

---
+++ Runtime/Editor/HierarchyRenamer.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace _UTIL_e
+{
+    sealed class HierarchyRenamer : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        //----------------------------------------------------------------------------------------------------------
+
+        [ContextMenu(nameof(Operate))]
+        void Operate()
+        {
+            Rename(transform, target);
+            static void Rename(in Transform self, in Transform target)
+            {
+                self.name = target.name;
+                int min = Mathf.Min(self.childCount, target.childCount);
+                for (int i = 0; i < min; ++i)
+                    Rename(self.GetChild(i), target.GetChild(i));
+            }
+        }
+    }
+}
+#endif
