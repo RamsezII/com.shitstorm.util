eaf99dbe46cf8be1b4c67bd065c47f1b0835dfb9
ram16 <thomass.montaine@gmail.com>
2025-09-11 15:36:52 +0200

    Update SettingsFile.cs

--- Runtime/Files/SettingsFile.cs
+++ Runtime/Files/SettingsFile.cs
@@ -5,15 +5,15 @@ namespace _UTIL_
 {
     public abstract class SettingsFile : JSon
     {
-        public string FILE_PATH => Path.Combine(Path.Combine(Application.streamingAssetsPath, typeof(SettingsFile).FullName).GetDir(true).FullName, GetType().FullName) + ".ark" + json;
+        public string GetFilePath() => Path.Combine(Path.Combine(Application.dataPath, "Resources").GetDir(true).FullName, GetJSonName(GetType()));
 
         //--------------------------------------------------------------------------------------------------------------
 
-        public void Save() => Save(FILE_PATH, true);
+        public void Save() => Save(GetFilePath(), true);
         public static void Load<T>(ref T text, in bool log) where T : SettingsFile, new()
         {
             text = new();
-            Read(ref text, text.FILE_PATH, true, log);
+            Read(ref text, text.GetFilePath(), true, log);
         }
     }
 }
