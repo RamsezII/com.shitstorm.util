8fa6a4243acb6f726c30c1c79f030ea2f2c29353
ram16 <thomass.montaine@gmail.com>
2025-09-28 18:44:42 +0200

    Update Util.math.cs

--- Runtime/Math/Util.math.cs
+++ Runtime/Math/Util.math.cs
@@ -5,10 +5,10 @@ public static partial class Util
     public static ushort LoopID(this ref byte id) => ++id == 0 ? ++id : id;
     public static ushort LoopID(this ref ushort id) => ++id == 0 ? ++id : id;
 
-    public static void Clamp(ref this float value, in float min, in float max) => Mathf.Clamp(value, min, max);
-    public static void Clamp01(ref this float value) => Mathf.Clamp01(value);
+    public static float Clamp_ref(ref this float value, in float min, in float max) => Mathf.Clamp(value, min, max);
+    public static float Clamp01_ref(ref this float value) => Mathf.Clamp01(value);
 
-    public static void ClampMagnitude(ref this Vector2 value, in float maxLength) => Vector2.ClampMagnitude(value, maxLength);
+    public static Vector2 ClampMagnitude_ref(ref this Vector2 value, in float maxLength) => Vector2.ClampMagnitude(value, maxLength);
 
     [System.Obsolete]
     public static int Repeat(this int value, int max)
