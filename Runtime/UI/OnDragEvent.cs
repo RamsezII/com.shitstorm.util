2de866b1aabe17574f090e7d8366645f7da4a48a
ram16 <thomass.montaine@gmail.com>
2025-10-15 21:17:58 +0200

    drag

---
+++ Runtime/UI/OnDragEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _UTIL_
+{
+    public sealed class OnDragEvent : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public Action<PointerEventData> onBeginDrag, onDrag, onEndDrag;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+        {
+            onBeginDrag?.Invoke(eventData);
+        }
+
+        void IDragHandler.OnDrag(PointerEventData eventData)
+        {
+            onDrag?.Invoke(eventData);
+        }
+
+        void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+        {
+            onEndDrag?.Invoke(eventData);
+        }
+    }
+}
---
+++ Runtime/UI/OnScrollEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _UTIL_
+{
+    public sealed class OnScrollEvent : MonoBehaviour, IScrollHandler
+    {
+        public Action<PointerEventData> onScroll;
+        void IScrollHandler.OnScroll(PointerEventData eventData)
+        {
+            onScroll?.Invoke(eventData);
+        }
+    }
+}
