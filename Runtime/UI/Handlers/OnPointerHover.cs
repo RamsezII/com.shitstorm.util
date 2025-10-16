6dcc784f43b93750eff203c3cdd74cd734ab4f10
ram16 <thomass.montaine@gmail.com>
2025-10-16 13:46:35 +0200

    ye

---
+++ Runtime/UI/Handlers/OnPointerHover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _UTIL_
+{
+    public sealed class OnPointerHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public Action<PointerEventData> onEnter, onExit;
+        public Action<PointerEventData, bool> onEnterExit;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+        {
+            onEnter?.Invoke(eventData);
+            onEnterExit?.Invoke(eventData, true);
+        }
+
+        void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+        {
+            onExit?.Invoke(eventData);
+            onEnterExit?.Invoke(eventData, false);
+        }
+    }
+}
---
+++ Runtime/UI/Handlers/OnPointerUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _UTIL_
+{
+    public sealed class OnPointerUp : MonoBehaviour, IPointerUpHandler
+    {
+        public Action<PointerEventData> onUp;
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+        {
+            onUp?.Invoke(eventData);
+        }
+    }
+}
