d12f3f25c8e1e08f3f44e29e953ec36e0d5dd1c3
ram16 <thomass.montaine@gmail.com>
2025-11-03 01:14:33 +0100

    Update Util_resources.cs

--- Runtime/Util/Other/Util_resources.cs
+++ Runtime/Util/Other/Util_resources.cs
@@ -3,6 +3,8 @@
 public static partial class Util
 {
     public static T LoadResourceByType<T>() where T : Object => Resources.Load<T>(typeof(T).FullName);
+    public static Object LoadResourceByType(in System.Type type) => Resources.Load(type.FullName, type);
+
     public static T InstantiateOrCreateIfAbsent<T>(in Transform parent = null, in FindObjectsInactive findObjectsInactive = FindObjectsInactive.Exclude) where T : MonoBehaviour
     {
         T clone = Object.FindAnyObjectByType<T>(findObjectsInactive);
