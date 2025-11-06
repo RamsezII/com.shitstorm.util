a2995d446b05aec3c3357737f89e3b369af30ffb
ram16 <thomass.montaine@gmail.com>
2025-11-06 18:59:23 +0100

    os platform util

---
+++ Runtime/OsPlatform/Util_os.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+partial class Util
+{
+    public static readonly bool is_app_windows = Application.platform.ToString().Contains("Windows", StringComparison.OrdinalIgnoreCase);
+    public static readonly bool is_app_linux = Application.platform.ToString().Contains("Linux", StringComparison.OrdinalIgnoreCase);
+    public static readonly bool is_os_windows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+    public static readonly bool is_os_linux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+}
