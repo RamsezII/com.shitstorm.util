f34006b5f8b13c917e7fd29492b10c850c480934
ram16 <thomass.montaine@gmail.com>
2025-11-07 14:56:39 +0100

    ready handler

---
+++ Runtime/Classes/ReadyHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _UTIL_
+{
+    public sealed class Readyhandler
+    {
+        public bool is_ready;
+        Action on_ready;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public void RegisterOnReady(in Action action)
+        {
+            if (is_ready)
+                action?.Invoke();
+            else
+                on_ready += action;
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public void Reset()
+        {
+            is_ready = false;
+            on_ready = null;
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public void TriggerReady()
+        {
+            is_ready = true;
+            on_ready?.Invoke();
+            on_ready = null;
+        }
+    }
+}
