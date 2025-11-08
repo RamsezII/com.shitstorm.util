3f1539f518fcfb067f2d3de2f79c7970630cf07e
ram16 <thomass.montaine@gmail.com>
2025-11-08 01:01:42 +0100

    contourner il2cpp et process start

---
+++ Runtime/Processes/Util_ShellExecuteW.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+partial class Util
+{
+#if UNITY_STANDALONE_WIN
+    // https://learn.microsoft.com/windows/win32/api/shellapi/nf-shellapi-shellexecutew
+    [System.Runtime.InteropServices.DllImport("shell32.dll", CharSet = System.Runtime.InteropServices.CharSet.Unicode, SetLastError = true)]
+    private static extern IntPtr ShellExecuteW(
+        IntPtr hwnd,
+        string lpOperation,   // "open" pour lancer
+        string lpFile,        // chemin .exe / .bat / .lnk
+        string lpParameters,  // arguments
+        string lpDirectory,   // working dir
+        int nShowCmd          // 1 = normal
+    );
+
+    public static void ShellExecute(string file, string args = "", string working_dir = "")
+    {
+        IntPtr result = ShellExecuteW(IntPtr.Zero, "open", file, args, working_dir, 1);
+        long code = result.ToInt64();
+        if (code <= 32)
+        {
+            throw new InvalidOperationException($"ShellExecuteW failed with code {code} when trying to open '{file}' with args '{args}' in dir '{working_dir}'.");
+        }
+    }
+#endif
+
+    [System.Runtime.InteropServices.DllImport("libc")]
+    public static extern int system(string cmd);
+}
--- Runtime/Strings/Util.logs.cs
+++ Runtime/Strings/Util.logs.cs
@@ -14,7 +14,8 @@ public static partial class Util
         return log.ToString();
     }
 
-    public static string TrimmedExceptionMessage(this Exception e) => e.Message.TrimEnd('\n', '\r', '\0');
+    public static string TrimmedExceptionMessage(this Exception e) => TrimmedExceptionMessage(e.Message);
+    public static string TrimmedExceptionMessage(this string message) => message.TrimEnd('\n', '\r', '\0');
     public static string FileSizeLog(this in uint fileSize) => ((long)fileSize).LogDataSize();
     public static string LogDataSize(this in long data_size)
     {
