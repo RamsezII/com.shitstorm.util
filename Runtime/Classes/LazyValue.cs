4433052f8fd108cf5ea199568e329c94dd422145
ram16 <thomass.montaine@gmail.com>
2025-11-21 00:30:26 +0100

    added LazyValue

---
+++ Runtime/Classes/LazyValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _UTIL_
+{
+    public sealed class LazyValue<T>
+    {
+        Func<T> factory;
+        T value;
+        bool ready;
+
+        //----------------------------------------------------------------------------------------------------------
+
+        public LazyValue(in Func<T> factory) => Reset(factory);
+
+        //----------------------------------------------------------------------------------------------------------
+
+        public T GetValue()
+        {
+            lock (this)
+                if (!ready)
+                {
+                    value = factory();
+                    ready = true;
+                }
+            return value;
+        }
+
+        public void Reset(in Func<T> factory)
+        {
+            this.factory = factory;
+            value = default;
+            ready = false;
+        }
+    }
+}
