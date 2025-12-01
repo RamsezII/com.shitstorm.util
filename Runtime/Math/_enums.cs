f17aae49a086ca867feff5ca29b5d9755be98f0d
ram16 <thomass.montaine@gmail.com>
2025-12-01 22:28:11 +0100

    dirs

---
+++ Runtime/Math/_enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _UTIL_
+{
+    public enum DIRS_ENUM : byte
+    {
+        Top,
+        Right,
+        Down,
+        Left,
+
+        TopRight,
+        DownRight,
+        DownLeft,
+        TopLeft,
+    }
+
+    [Flags]
+    public enum DIRS_FLAGS : byte
+    {
+        Top = 1 << DIRS_ENUM.Top,
+        Right = 1 << DIRS_ENUM.Right,
+        Down = 1 << DIRS_ENUM.Down,
+        Left = 1 << DIRS_ENUM.Left,
+
+        TopRight = Top | Right,
+        DownRight = Down | Right,
+        DownLeft = Down | Left,
+        TopLeft = Top | Left,
+    }
+}
