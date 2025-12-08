6f4b5ad8e0ffa50fc38c34de35012d85be4c2f37
ram16 <thomass.montaine@gmail.com>
2025-12-08 02:39:15 +0100

    Update Util_transforms.cs

--- Runtime/Transforms/Util_transforms.cs
+++ Runtime/Transforms/Util_transforms.cs
@@ -3,4 +3,5 @@
 partial class Util
 {
     public static Vector3 TransformPoint_unscaled(this Transform transform, in Vector3 position) => transform.position + transform.rotation * position;
+    public static Vector3 InverseTransformPoint_unscaled(this Transform transform, in Vector3 position) => Quaternion.Inverse(transform.rotation) * (position - transform.position);
 }
