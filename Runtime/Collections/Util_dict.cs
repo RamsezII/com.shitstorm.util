d7460cd94fec45f0bc09183822baef7b7ab2771c
ram16 <thomass.montaine@gmail.com>
2025-12-14 23:39:03 +0100

    ye

---
+++ Runtime/Collections/Util_dict.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+partial class Util
+{
+    public static Dictionary<string, T> EnumToDict<T>(in IEqualityComparer<string> comparer) where T : struct, Enum
+    {
+        var type = typeof(T);
+        var names = Enum.GetNames(type);
+        var values = (T[])Enum.GetValues(type);
+
+        var dict = new Dictionary<string, T>(names.Length, comparer);
+        for (int i = 0; i < names.Length; i++)
+            dict[names[i]] = values[i];
+
+        return dict;
+    }
+
+    public static Dictionary<string, object> EnumToDict(in Type enumType, in IEqualityComparer<string> comparer)
+    {
+        if (enumType is null) throw new ArgumentNullException(nameof(enumType));
+        if (!enumType.IsEnum)
+            throw new ArgumentException($"expected enum, got {enumType.FullName}", nameof(enumType));
+
+        var names = Enum.GetNames(enumType);
+        var values = Enum.GetValues(enumType); // Array non-générique
+
+        var dict = new Dictionary<string, object>(names.Length, comparer);
+        for (int i = 0; i < names.Length; i++)
+            dict[names[i]] = values.GetValue(i)!;
+
+        return dict;
+    }
+}
