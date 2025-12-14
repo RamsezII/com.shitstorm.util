9a9337de9ae47b98afdf3b420ef33340f60f5b2a
ram16 <thomass.montaine@gmail.com>
2025-12-14 01:38:45 +0100

    NOWRAP

---
+++ Runtime/Strings/_character_wrap.cs
@@ -0,0 +1,31 @@
+﻿partial class Util
+{
+    public const char
+        PROMPT_CHAR = '»',
+        NOWRAP_CHAR = '\u00A0';
+
+    //----------------------------------------------------------------------------------------------------------
+
+    public static string AllSpaceToNowrap(this string text) => HasSpaces(text) ? text.Replace(' ', NOWRAP_CHAR) : text;
+    public static string AllNowrapToSpace(this string text) => text.Contains(NOWRAP_CHAR) ? text.Replace(NOWRAP_CHAR, ' ') : text;
+
+    public static bool HasSpaces(this string text) => text.Contains2("\t ");
+
+    public static bool ForceUnwrappable(ref string text)
+    {
+        if (!HasSpaces(text))
+            return true;
+        text = text.Replace(' ', NOWRAP_CHAR);
+        return false;
+    }
+
+    public static bool RemoveNowraps(ref string text)
+    {
+        if (text.Contains(NOWRAP_CHAR))
+        {
+            text = text.Remove(NOWRAP_CHAR, ' ');
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Runtime/Strings/_character_wrap_e.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using TMPro;
+using UnityEditor;
+
+static partial class Util_zspaces_e
+{
+    const string button_prefixe = "CONTEXT/" + nameof(TMP_InputField) + "/";
+
+    //----------------------------------------------------------------------------------------------------------
+
+    [MenuItem(button_prefixe + nameof(AllSpaceToNowrap))]
+    static void AllSpaceToNowrap(MenuCommand command)
+    {
+        var inputfield = (TMP_InputField)command.context;
+        string new_text = inputfield.text.AllSpaceToNowrap();
+        inputfield.text = new_text;
+    }
+
+    [MenuItem(button_prefixe + nameof(AllNowrapToSpace))]
+    static void AllNowrapToSpace(MenuCommand command)
+    {
+        var inputfield = (TMP_InputField)command.context;
+        string new_text = inputfield.text.AllNowrapToSpace();
+        inputfield.text = new_text;
+    }
+}
+#endif
