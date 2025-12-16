597033490797b58b1b960da2b3a548c5afa764cb
ram16 <thomass.montaine@gmail.com>
2025-12-16 23:33:22 +0100

    Update Util_go.cs

--- Runtime/GameObjects/Util_go.cs
+++ Runtime/GameObjects/Util_go.cs
@@ -2,5 +2,6 @@
 
 partial class Util
 {
+    public static GameObject Clone(this GameObject prefab) => Object.Instantiate(prefab, prefab.transform.parent);
     public static T Clone<T>(this T prefab) where T : MonoBehaviour => Object.Instantiate(prefab, prefab.transform.parent);
 }
