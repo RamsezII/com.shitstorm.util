83b0051daeef6fd4f2fef06e4c80d03f3baffc06
ram16 <thomass.montaine@gmail.com>
2025-12-16 03:30:08 +0100

    yes

--- Runtime/Collections/Util_collections.cs
+++ Runtime/Collections/Util_collections.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 
 partial class Util
 {
+    public static T Pop<T>(this List<T> list)
+    {
+        T last = list[^1];
+        list.RemoveAt(list.Count - 1);
+        return last;
+    }
+
     public static Dictionary<string, T> EnumToDict<T>(in IEqualityComparer<string> comparer) where T : struct, Enum
     {
         var type = typeof(T);
