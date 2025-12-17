9ded1e8e990b8cb65d0472255a3663aa251eaf4a
ram16 <thomass.montaine@gmail.com>
2025-12-17 02:02:51 +0100

    ye

--- Runtime/Strings/Util_sb.cs
+++ Runtime/Strings/Util_sb.cs
@@ -1,48 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using UnityEngine;
 
 partial class Util
 {
     public static string TroncatedForLog(this StringBuilder sb) => sb == null || sb.Length == 0 ? string.Empty : sb.ToString()[..^1];
     public static void Log(this StringBuilder sb, in UnityEngine.Object o = null) => Debug.Log(TroncatedForLog(sb), o);
-
-    public static string PullValue(this StringBuilder sb)
-    {
-        string value = sb.ToString();
-        sb.Clear();
-        return value;
-    }
-
-    public static string[] TextToLines(this string text)
-    {
-        if (string.IsNullOrWhiteSpace(text))
-            return new string[0];
-        return text.Split(new[] { "\r\n", "\r", "\n", }, StringSplitOptions.RemoveEmptyEntries);
-    }
-
-    public static object[] ExtractDataArray(this object data) => data switch
-    {
-        string str => str.TextToLines(),
-        object[] array => array,
-        IEnumerable objects => objects.Cast<object>().ToArray(),
-        _ => new[] { data, },
-    };
-
-    public static IEnumerable<object> IterateThroughData(this object data) => data switch
-    {
-        string str => str.TextToLines(),
-        IEnumerable objects => objects.Cast<object>(),
-        _ => new[] { data, },
-    };
-
-    public static IEnumerable<string> IterateThroughData_str(this object data) => data switch
-    {
-        string str => str.TextToLines(),
-        IEnumerable objects => objects.Cast<object>().Select(x => x?.ToString()),
-        _ => new[] { data?.ToString(), },
-    };
 }
---
+++ Runtime/Strings/Util_strings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+partial class Util
+{
+    public static string RemoveChar(this string input, char c)
+    {
+        StringBuilder sb = new();
+        for (int i = 0; i < input.Length; i++)
+            if (input[i] != c)
+                sb.Append(input[i]);
+        return sb.ToString();
+    }
+
+    public static string RemoveChars(this string input, string chars)
+    {
+        HashSet<char> _chars = new(chars);
+        StringBuilder sb = new();
+        for (int i = 0; i < input.Length; i++)
+            if (!_chars.Contains(input[i]))
+                sb.Append(input[i]);
+        return sb.ToString();
+    }
+}
