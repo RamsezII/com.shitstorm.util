c64a1b31a47fc5ccca3ce5b8562bf5cd61589858
ram16 <thomass.montaine@gmail.com>
2025-12-19 01:44:22 +0100

    ye

--- Runtime/GameObjects/Util_go.cs
+++ Runtime/GameObjects/Util_go.cs
@@ -3,5 +3,20 @@
 partial class Util
 {
     public static GameObject Clone(this GameObject prefab) => Object.Instantiate(prefab, prefab.transform.parent);
+    public static GameObject Clone(this GameObject prefab, in bool set_active)
+    {
+        GameObject clone = Object.Instantiate(prefab, prefab.transform.parent);
+        if (set_active)
+            clone.SetActive(true);
+        return clone;
+    }
+
     public static T Clone<T>(this T prefab) where T : MonoBehaviour => Object.Instantiate(prefab, prefab.transform.parent);
+    public static T Clone<T>(this T prefab, in bool set_active) where T : MonoBehaviour
+    {
+        T clone = Object.Instantiate(prefab, prefab.transform.parent);
+        if (set_active)
+            clone.gameObject.SetActive(true);
+        return clone;
+    }
 }
---
+++ Runtime/UI/UI_bounds.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+partial class Util
+{
+    public static bool WorldBoundsToScreenRect(
+        in Bounds bounds,
+        in Camera camera,
+        out Rect screenRect
+    )
+    {
+        screenRect = default;
+
+        Vector3 c = bounds.center;
+        Vector3 e = bounds.extents;
+
+        Vector3[] corners =
+        {
+            c + new Vector3(+e.x, +e.y, +e.z),
+            c + new Vector3(+e.x, +e.y, -e.z),
+            c + new Vector3(+e.x, -e.y, +e.z),
+            c + new Vector3(+e.x, -e.y, -e.z),
+            c + new Vector3(-e.x, +e.y, +e.z),
+            c + new Vector3(-e.x, +e.y, -e.z),
+            c + new Vector3(-e.x, -e.y, +e.z),
+            c + new Vector3(-e.x, -e.y, -e.z),
+        };
+
+        Vector2 min = new(float.PositiveInfinity, float.PositiveInfinity);
+        Vector2 max = new(float.NegativeInfinity, float.NegativeInfinity);
+        bool anyInFront = false;
+
+        for (int i = 0; i < corners.Length; i++)
+        {
+            Vector3 w = corners[i];
+            Vector3 sp = camera.WorldToViewportPoint(w);
+
+            if (sp.z <= 0)
+                continue;
+
+            anyInFront = true;
+            min = Vector2.Min(min, sp);
+            max = Vector2.Max(max, sp);
+        }
+
+        if (!anyInFront)
+            return false;
+
+        screenRect = Rect.MinMaxRect(min.x, min.y, max.x, max.y);
+        return true;
+    }
+}
