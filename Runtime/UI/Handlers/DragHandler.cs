b4bf75cc216562bf67fb93fd7dd1299a53f8993f
ram16 <thomass.montaine@gmail.com>
2025-12-21 04:14:12 +0100

    bool dragging dans DragHandler

--- Runtime/UI/Handlers/DragHandler.cs
+++ Runtime/UI/Handlers/DragHandler.cs
@@ -6,11 +6,13 @@ namespace _UTIL_
     public sealed class DragHandler : PointerHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         public Action<PointerEventData> onBeginDrag, onDrag, onEndDrag;
+        public bool dragging;
 
         //--------------------------------------------------------------------------------------------------------------
 
         void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
         {
+            dragging = true;
             onBeginDrag?.Invoke(eventData);
             if (propagateToParent)
                 transform.parent.GetComponentInParent<IBeginDragHandler>()?.OnBeginDrag(eventData);
@@ -25,6 +27,7 @@ namespace _UTIL_
 
         void IEndDragHandler.OnEndDrag(PointerEventData eventData)
         {
+            dragging = false;
             onEndDrag?.Invoke(eventData);
             if (propagateToParent)
                 transform.parent.GetComponentInParent<IEndDragHandler>()?.OnEndDrag(eventData);
