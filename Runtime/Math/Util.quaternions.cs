58e54418c4169bb4236c03665c514841007bf7ac
ram16 <thomass.montaine@gmail.com>
2026-02-05 14:01:30 +0100

    Update Util.quaternions.cs

--- Runtime/Math/Util.quaternions.cs
+++ Runtime/Math/Util.quaternions.cs
@@ -22,5 +22,11 @@ public static partial class Util
         return angle * axis;
     }
 
+    public static void FromToRotationAxisAngle(this in Quaternion A, in Quaternion B, out Vector3 axis, out float angle)
+    {
+        Quaternion deltaRotation = B * Quaternion.Inverse(A);
+        deltaRotation.ToAngleAxis(out angle, out axis);
+    }
+
     public static Quaternion RotateTowards(this in Quaternion rotation, in Vector3 from, in Vector3 to) => Quaternion.FromToRotation(rotation * from, to) * rotation;
 }
