1d6d68c13fe24386e3ed34a012325839bdb53179
ram16 <thomass.montaine@gmail.com>
2026-02-09 03:01:28 +0100

    renderers

---
+++ Runtime/UI/Renderers/UI_GradientRenderer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _UTIL_
+{
+    public sealed class UI_GradientRenderer : Graphic
+    {
+        [Serializable]
+        public struct Pair
+        {
+            public float position;
+            public Color color;
+            public Pair(in float position, in Color color)
+            {
+                this.position = position;
+                this.color = color;
+            }
+        }
+
+        public List<Pair> values = new()
+        {
+            new Pair(0f, Color.red),
+            new Pair(.5f, Color.green),
+            new Pair(1f, Color.blue),
+        };
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        [ContextMenu(nameof(SetVerticesDirty))]
+        public override void SetVerticesDirty() => base.SetVerticesDirty();
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            base.OnPopulateMesh(vh);
+
+            vh.Clear();
+
+            if (values.Count < 2)
+                return;
+
+            Rect r = rectTransform.rect;
+            UIVertex vertex = UIVertex.simpleVert;
+
+            var first = values[0];
+            var last = values[^1];
+
+            vertex.position = .5f * new Vector2(-r.width, -r.height);
+            vertex.color = color * first.color;
+            vh.AddVert(vertex);
+
+            vertex.position = .5f * new Vector2(-r.width, r.height);
+            vertex.color = color * first.color;
+            vh.AddVert(vertex);
+
+            for (int i = 1; i < values.Count; i++)
+            {
+                var pair = values[i];
+                float x = r.width * (-.5f + Mathf.InverseLerp(first.position, last.position, pair.position));
+
+                vertex.position = new Vector2(x, -.5f * r.height);
+                vertex.color = color * pair.color;
+                vh.AddVert(vertex);
+
+                vertex.position = new Vector2(x, .5f * r.height);
+                vertex.color = color * pair.color;
+                vh.AddVert(vertex);
+
+                int i2 = 2 + i * 2;
+
+                vh.AddTriangle(i2 - 4, i2 - 3, i2 - 2);
+                vh.AddTriangle(i2 - 3, i2 - 1, i2 - 2);
+            }
+        }
+    }
+}
--- Runtime/UI/Renderers/UI_RectRenderer.cs
+++ Runtime/UI/Renderers/UI_RectRenderer.cs
@@ -5,30 +5,44 @@ namespace _UTIL_
 {
     public sealed class UI_RectRenderer : Graphic
     {
+        public Color
+            top_left = Color.green,
+            top_right = Color.red,
+            bottom_left = Color.blue,
+            bottom_right = Color.yellow;
+
+        //--------------------------------------------------------------------------------------------------------------
+
         [ContextMenu(nameof(SetVerticesDirty))]
         public override void SetVerticesDirty() => base.SetVerticesDirty();
 
         protected override void OnPopulateMesh(VertexHelper vh)
         {
-            //base.OnPopulateMesh(vh);
+            base.OnPopulateMesh(vh);
 
             vh.Clear();
 
             Rect r = rectTransform.rect;
             UIVertex vertex = UIVertex.simpleVert;
-            vertex.color = color;
 
-            vertex.position = new(0, 0);
+            vertex.position = .5f * new Vector2(-r.width, r.height);
+            vertex.color = color * top_left;
             vh.AddVert(vertex);
-            vertex.position = new(r.width, 0);
+
+            vertex.position = .5f * new Vector2(r.width, r.height);
+            vertex.color = color * top_right;
             vh.AddVert(vertex);
-            vertex.position = new(r.width, r.height);
+
+            vertex.position = .5f * new Vector2(r.width, -r.height);
+            vertex.color = color * bottom_right;
             vh.AddVert(vertex);
-            vertex.position = new(0, r.height);
+
+            vertex.position = .5f * new Vector2(-r.width, -r.height);
+            vertex.color = color * bottom_left;
             vh.AddVert(vertex);
 
             vh.AddTriangle(0, 1, 2);
-            vh.AddTriangle(0, 2, 3);
+            vh.AddTriangle(2, 3, 0);
         }
     }
 }
