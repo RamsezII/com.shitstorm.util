274e37438675b0978750435c061462932e1384ee
ram16 <thomass.montaine@gmail.com>
2026-02-14 18:26:05 +0100

    Update Util.threads.cs

--- Runtime/Util/Other/Util.threads.cs
+++ Runtime/Util/Other/Util.threads.cs
@@ -2,6 +2,11 @@ using System.Threading;
 
 public static partial class Util
 {
-    public static void Lock(this object self) => Monitor.Enter(self);
-    public static void Unlock(this object self) => Monitor.Exit(self);
+    public static void Lock(in object self) => Monitor.Enter(self);
+    public static void Unlock(in object self) => Monitor.Exit(self);
+    public static void UnlockAndLock(in object self)
+    {
+        Monitor.Exit(self);
+        Monitor.Enter(self);
+    }
 }
