aee49dcc4c16a22ffec0ae61a0c6a1043ad52c03
ram16 <thomass.montaine@gmail.com>
2026-02-17 02:04:37 +0100

    Update Util.vector.cs

--- Runtime/Math/Util.vector.cs
+++ Runtime/Math/Util.vector.cs
@@ -2,10 +2,6 @@
 
 public static partial class Util
 {
-    public static float Max(this in Vector3 value) => Mathf.Max(value.x, value.y, value.z);
-    public static float Manhattan(this in Vector3 a, in Vector3 b) => Mathf.Max(Mathf.Abs(b.x - a.x), Mathf.Abs(b.y - a.y), Mathf.Abs(b.z - a.z));
-    public static float ToWeight(this in Vector3 weight) => Mathf.Clamp(weight.x, weight.y, weight.z);
-
     public static Vector3 SignedEuler_OLD(this Vector3 value)
     {
         for (int i = 0; i < 3; i++)
@@ -15,11 +11,8 @@ public static partial class Util
             else if (value[i] < -180)
                 value[i] += 360;
         }
-
         return value;
     }
 
-    public static Vector2 GetAbsoluteValue(this in Vector2 value) => new(Mathf.Abs(value.x), Mathf.Abs(value.y));
     public static Vector3 GetAbsoluteValue(this in Vector3 value) => new(Mathf.Abs(value.x), Mathf.Abs(value.y), Mathf.Abs(value.z));
-    public static Vector4 GetAbsoluteValue(this in Vector4 value) => new(Mathf.Abs(value.x), Mathf.Abs(value.y), Mathf.Abs(value.z), Mathf.Abs(value.w));
-}
+ }
