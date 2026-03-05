d3a22b7f5dcbd835464f64dfdbb3322a5c321cc7
ram16 <thomass.montaine@gmail.com>
2026-03-05 02:43:04 +0100

    Update OnStateMachine.cs

--- Runtime/Animator/OnStateMachine.cs
+++ Runtime/Animator/OnStateMachine.cs
@@ -9,31 +9,15 @@ namespace _UTIL_
 
     public class OnStateMachine : StateMachineBehaviour
     {
-        [SerializeField] bool defaut_b;
-
-        //----------------------------------------------------------------------------------------------------------
-
-        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            if (defaut_b)
-                animator.SetLayerWeight(layerIndex, 0);
-            OnState(animator, stateInfo, layerIndex, true);
-        }
-
-        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            if (defaut_b)
-                animator.SetLayerWeight(layerIndex, 1);
-            OnState(animator, stateInfo, layerIndex, false);
-        }
-
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) => OnState(animator, stateInfo, layerIndex, true);
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) => OnState(animator, stateInfo, layerIndex, false);
         void OnState(in Animator animator, in AnimatorStateInfo stateInfo, in int layerIndex, in bool onEnter)
         {
-            IOnStateMachine iUser = animator.GetComponentInParent<IOnStateMachine>();
-            if (iUser == null)
+            IOnStateMachine user = animator.GetComponentInParent<IOnStateMachine>(true);
+            if (user == null)
                 Debug.LogWarning($"{typeof(IOnStateMachine).FullName} not found on: {animator.transform.GetPath(true)}");
             else
-                iUser.OnStateMachine(stateInfo, layerIndex, onEnter);
+                user.OnStateMachine(stateInfo, layerIndex, onEnter);
         }
     }
 }
