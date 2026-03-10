833fcce0cc07acfdb5ebbfa5e06e7136b21d3fee
ram16 <thomass.montaine@gmail.com>
2026-03-10 18:34:18 +0100

    Update Util_njson.cs

--- Runtime/NJSon/Util_njson.cs
+++ Runtime/NJSon/Util_njson.cs
@@ -21,7 +21,7 @@ partial class Util
             Debug.Log($"NJSave({path})".ToSubLog());
     }
 
-    public static bool NJRead<T>(this string path, out T njson, in bool log_success = true, in bool log_failure = true) where T : JToken, new()
+    public static bool TryNJRead<T>(this string path, out T njson, in bool log_success = true, in bool log_failure = true) where T : JToken, new()
     {
         if (File.Exists(path))
         {
@@ -30,12 +30,12 @@ partial class Util
                 string text = File.ReadAllText(path);
                 njson = JsonConvert.DeserializeObject<T>(text);
                 if (log_success)
-                    Debug.Log($"{nameof(NJRead)}({path})".ToSubLog());
+                    Debug.Log($"{nameof(TryNJRead)}({path})".ToSubLog());
                 return true;
             }
             catch (Exception e)
             {
-                Debug.LogWarning($"ERROR {nameof(NJRead)}: \"{e.TrimmedExceptionMessage()}\" ({path})");
+                Debug.LogWarning($"ERROR {nameof(TryNJRead)}: \"{e.TrimmedExceptionMessage()}\" ({path})");
                 njson = new();
                 return false;
             }
@@ -43,7 +43,7 @@ partial class Util
         else
         {
             if (log_failure)
-                Debug.LogWarning($"{nameof(NJRead)} no NJSon at: \"{path}\"");
+                Debug.LogWarning($"{nameof(TryNJRead)} no NJSon at: \"{path}\"");
             njson = new();
             return false;
         }
