defc0bbbacfbbfb7909cb406ed1edcea07b11e87
ram16 <thomass.montaine@gmail.com>
2026-03-23 15:23:43 +0100

    transform inspector

---
+++ Runtime/Editor_old/Util_e/Transform_inspector.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace _EDITOR_
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Transform))]
+    public class Transform_inspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            Transform rT = (Transform)target;
+
+            if (GUILayout.Button(nameof(Util.GetPath)))
+                Debug.Log(GUIUtility.systemCopyBuffer = rT.GetPath(true));
+            if (GUILayout.Button(nameof(Util_e_OLD.LogTypes)))
+                rT.LogTypes();
+        }
+    }
+}
+#endif
