21b3cae00d7cad5c8d859cf34c38c84fa1a79244
ram16 <thomass.montaine@gmail.com>
2026-08-11 23:10:08 +0200

    edit utils

---
+++ Editor/RectTransforms.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace _UTIL_.Editor
+{
+    static class RectTransforms
+    {
+        [MenuItem("CONTEXT/" + nameof(RectTransform) + "/" + nameof(LogSize))]
+        static void LogSize(MenuCommand command)
+        {
+            RectTransform rt = (RectTransform)command.context;
+            Debug.Log(rt.rect.size);
+        }
+    }
+}
+#endif
