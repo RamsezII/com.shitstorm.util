1e042cd2ef90b407f15d12499cd07399dc5d565d
ram16 <thomass.montaine@gmail.com>
2026-08-22 23:09:09 +0200

    Update Util_sb.cs

--- Runtime/Strings/Util_sb.cs
+++ Runtime/Strings/Util_sb.cs
@@ -1,8 +1,19 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using UnityEngine;
 
 partial class Util
 {
-    public static string TroncatedForLog(this StringBuilder sb) => sb == null || sb.Length == 0 ? string.Empty : sb.ToString()[..^1];
-    public static void Log(this StringBuilder sb, in Object o = null) => Debug.Log(TroncatedForLog(sb), o);
+    public static void Log(this StringBuilder sb, in UnityEngine.Object o = null) => Debug.Log(TroncatedForLog(sb), o);
+    public static string TroncatedForLog(this StringBuilder sb)
+    {
+        if (sb == null || sb.Length == 0)
+            return string.Empty;
+
+        string text = sb.ToString();
+
+        return text.EndsWith(Environment.NewLine, StringComparison.Ordinal)
+            ? text[..^Environment.NewLine.Length]
+            : text;
+    }
 }
