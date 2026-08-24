a5ccedf802013cdb84fad7aba5f81bbe46cb7fd4
ram16 <thomass.montaine@gmail.com>
2026-08-24 03:27:23 +0200

    rigs

---
+++ Runtime/RigAnim/Util_riganim.cs
@@ -0,0 +1,13 @@
+﻿#if HAS_ANIMRIG
+using UnityEngine.Animations.Rigging;
+
+partial class Util
+{
+    public static void AddWeightedTransform(this MultiParentConstraint rigConstraint, in WeightedTransform weightedTransform)
+    {
+        var sourcesR = rigConstraint.data.sourceObjects;
+        sourcesR.Add(weightedTransform);
+        rigConstraint.data.sourceObjects = sourcesR;
+    }
+}
+#endif
