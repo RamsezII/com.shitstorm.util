293b560062b0f2b7148dc80e119aecee1b85cf35
ram16 <thomass.montaine@gmail.com>
2026-08-29 14:03:13 +0200

    Update NJSon.cs

--- Runtime/NJSon/NJSon.cs
+++ Runtime/NJSon/NJSon.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using _UTIL_;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
@@ -8,120 +7,102 @@ using UnityEngine;
 namespace _UTIL_
 {
     [AttributeUsage(AttributeTargets.Field)]
-    public sealed class NJFieldAttribute : Attribute
-    {
-    }
-
-    [AttributeUsage(AttributeTargets.Field)]
     public sealed class NJTransformAttribute : Attribute
     {
     }
-}
 
-partial class Util
-{
-    public const BindingFlags BindingFlagsALL =
-        BindingFlags.Public |
-        BindingFlags.NonPublic |
-        BindingFlags.Instance |
-        BindingFlags.Static;
-
-    //----------------------------------------------------------------------------------------------------------
-
-    public static void TryRead_out<T>(this JObject jobj, string name, out T value)
+    public static partial class NJSon
     {
-        value = default;
-        JToken token = jobj[name];
-        if (token != null)
-            value = token.ToObject<T>();
-    }
+        public const BindingFlags BindingFlagsALL =
+            BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.Instance |
+            BindingFlags.Static;
 
-    public static void TryRead_ref<T>(this JObject jobj, string name, ref T value)
-    {
-        JToken token = jobj[name];
-        if (token != null)
-            value = token.ToObject<T>();
-    }
+        //----------------------------------------------------------------------------------------------------------
 
-    public static void WriteNJFields(this JObject jobj, in object target) => WriteFields<NJFieldAttribute>(jobj, target, target.GetType());
-    public static void WriteFields<T>(this JObject jobj, in object target, in Type type = null) where T : Attribute
-    {
-        foreach (var field in (type ?? target.GetType()).GetFields(BindingFlagsALL))
+        public static void WriteFields<T>(this JObject jobj, in object target = null, in Type type = null) where T : Attribute
         {
-            var attr = field.GetCustomAttribute<T>();
-            if (attr == null)
-                continue;
-
-            object value = field.GetValue(target);
-            jobj[field.Name] = value == null ? JValue.CreateNull() : JToken.FromObject(value);
-        }
-    }
-
-    public static void ReadNJFields(this JObject jobj, in object target) => ReadFields<NJFieldAttribute>(jobj, target, target.GetType());
-    public static void ReadFields<T>(this JObject jobj, in object target, in Type type = null) where T : Attribute
-    {
-        foreach (var field in (type ?? target.GetType()).GetFields(BindingFlagsALL))
-        {
-            var attr = field.GetCustomAttribute<T>();
-            if (attr == null)
-                continue;
-
-            if (!jobj.TryGetValue(field.Name, out JToken token))
-                continue;
-
-            if (token.Type != JTokenType.Null)
-                field.SetValue(target, token.ToObject(field.FieldType));
-            else
+            foreach (var field in (type ?? target.GetType()).GetFields(BindingFlagsALL))
             {
-                // null autorisé uniquement pour références et Nullable<T>
-                if (!field.FieldType.IsValueType || Nullable.GetUnderlyingType(field.FieldType) != null)
-                    field.SetValue(target, null);
-            }
-        }
-    }
-
-    // -------------------------------------------------------------------------------------------------------------
-
-    public static void WriteNJTransforms(this JObject jobj, object target, in Transform root)
-    {
-        foreach (var field in target.GetType().GetFields(BindingFlagsALL))
-            if (field.FieldType == typeof(Transform) && field.GetValue(target) is Transform transform)
-            {
-                var attr = field.GetCustomAttribute<NJTransformAttribute>();
+                var attr = field.GetCustomAttribute<T>();
                 if (attr == null)
                     continue;
 
-                jobj[field.Name] = new JObject()
+                object value = field.GetValue(target);
+                jobj[field.Name] = value == null ? JValue.CreateNull() : JToken.FromObject(value);
+            }
+        }
+
+        public static void ReadFields<T>(this JObject jobj, in object target = null, in Type type = null) where T : Attribute
+        {
+            foreach (var field in (type ?? target.GetType()).GetFields(BindingFlagsALL))
+            {
+                var attr = field.GetCustomAttribute<T>();
+                if (attr == null)
+                    continue;
+
+                if (!jobj.TryGetValue(field.Name, out JToken token))
+                    continue;
+
+                if (token.Type != JTokenType.Null)
+                    field.SetValue(target, token.ToObject(field.FieldType));
+                else
                 {
-                    [nameof(transform.localPosition)] = JsonConvert.SerializeObject(transform.localPosition),
-                    [nameof(transform.localEulerAngles)] = JsonConvert.SerializeObject(transform.localEulerAngles.SignedEulers()),
-                    ["relativePath"] = transform.GetRelativePath(root),
-                };
+                    // null autorisé uniquement pour références et Nullable<T>
+                    if (!field.FieldType.IsValueType || Nullable.GetUnderlyingType(field.FieldType) != null)
+                        field.SetValue(target, null);
+                }
             }
-    }
+        }
 
-    public static void ReadNJTransforms(this JObject jobj, object target, in Transform root)
-    {
-        foreach (var field in target.GetType().GetFields(BindingFlagsALL))
-            if (field.FieldType == typeof(Transform))
-            {
-                var attr = field.GetCustomAttribute<NJTransformAttribute>();
-                if (attr == null)
-                    continue;
+        // -------------------------------------------------------------------------------------------------------------
 
-                if (jobj[field.Name] is JObject jtfm)
-                    if (jtfm.TryGetValue("relativePath", out var jpath))
+        public static void WriteNJTransforms(this JObject jobj, object target, in Transform root)
+        {
+            foreach (var field in target.GetType().GetFields(BindingFlagsALL))
+                if (field.FieldType == typeof(Transform) && field.GetValue(target) is Transform transform)
+                {
+                    var attr = field.GetCustomAttribute<NJTransformAttribute>();
+                    if (attr == null)
+                        continue;
+
+                    jobj[field.Name] = new JObject()
                     {
-                        Transform transform = root.ForceFind((string)jpath, false);
+                        [nameof(transform.localPosition)] = JsonConvert.SerializeObject(transform.localPosition),
+                        [nameof(transform.localEulerAngles)] = JsonConvert.SerializeObject(transform.localEulerAngles.SignedEulers()),
+                        [nameof(transform.localScale)] = JsonConvert.SerializeObject(transform.localScale),
+                        ["relativePath"] = transform.GetRelativePath(root),
+                    };
+                }
+        }
 
-                        if (jtfm.TryGetValue(nameof(transform.localPosition), out var jpos))
-                            transform.localPosition = jpos.ToObject<Vector3>();
+        public static void ReadNJTransforms(this JObject jobj, object target, in Transform root)
+        {
+            foreach (var field in target.GetType().GetFields(BindingFlagsALL))
+                if (field.FieldType == typeof(Transform))
+                {
+                    var attr = field.GetCustomAttribute<NJTransformAttribute>();
+                    if (attr == null)
+                        continue;
 
-                        if (jtfm.TryGetValue(nameof(transform.localEulerAngles), out var jeul))
-                            transform.localEulerAngles = jeul.ToObject<Vector3>();
+                    if (jobj[field.Name] is JObject jtfm)
+                        if (jtfm.TryGetValue("relativePath", out var jpath))
+                        {
+                            Transform transform = root.ForceFind((string)jpath, false);
 
-                        field.SetValue(target, transform);
-                    }
-            }
+                            if (jtfm.TryGetValue(nameof(transform.localPosition), out var jpos))
+                                transform.localPosition = jpos.ToObject<Vector3>();
+
+                            if (jtfm.TryGetValue(nameof(transform.localEulerAngles), out var jeul))
+                                transform.localEulerAngles = jeul.ToObject<Vector3>();
+
+                            if (jtfm.TryGetValue(nameof(transform.localScale), out var jscale))
+                                transform.localScale = jscale.ToObject<Vector3>();
+
+                            field.SetValue(target, transform);
+                        }
+                }
+        }
     }
 }
