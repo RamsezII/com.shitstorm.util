8008b319cbeefd3f7137f9662d9cb527bb9250bb
ram16 <thomass.montaine@gmail.com>
2026-08-29 13:59:56 +0200

    correction again

--- Runtime/NJSon/NJSon.cs
+++ Runtime/NJSon/NJSon.cs
@@ -28,6 +28,21 @@ partial class Util
 
     //----------------------------------------------------------------------------------------------------------
 
+    public static void TryRead_out<T>(this JObject jobj, string name, out T value)
+    {
+        value = default;
+        JToken token = jobj[name];
+        if (token != null)
+            value = token.ToObject<T>();
+    }
+
+    public static void TryRead_ref<T>(this JObject jobj, string name, ref T value)
+    {
+        JToken token = jobj[name];
+        if (token != null)
+            value = token.ToObject<T>();
+    }
+
     public static void WriteNJFields(this JObject jobj, in object target) => WriteFields<NJFieldAttribute>(jobj, target, target.GetType());
     public static void WriteFields<T>(this JObject jobj, in object target, in Type type = null) where T : Attribute
     {
@@ -43,12 +58,11 @@ partial class Util
     }
 
     public static void ReadNJFields(this JObject jobj, in object target) => ReadFields<NJFieldAttribute>(jobj, target, target.GetType());
-    public static void ReadFields<T>(this JObject jobj, in object target, Type type = null) where T : Attribute
+    public static void ReadFields<T>(this JObject jobj, in object target, in Type type = null) where T : Attribute
     {
-        type ??= target.GetType();
-        foreach (var field in type.GetFields(BindingFlagsALL))
+        foreach (var field in (type ?? target.GetType()).GetFields(BindingFlagsALL))
         {
-            var attr = field.GetCustomAttribute<NJFieldAttribute>();
+            var attr = field.GetCustomAttribute<T>();
             if (attr == null)
                 continue;
 
@@ -56,11 +70,11 @@ partial class Util
                 continue;
 
             if (token.Type != JTokenType.Null)
-                field.SetValue(target, token.ToObject(type));
+                field.SetValue(target, token.ToObject(field.FieldType));
             else
             {
                 // null autorisé uniquement pour références et Nullable<T>
-                if (!type.IsValueType || Nullable.GetUnderlyingType(type) != null)
+                if (!field.FieldType.IsValueType || Nullable.GetUnderlyingType(field.FieldType) != null)
                     field.SetValue(target, null);
             }
         }
