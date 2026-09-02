718345d1b54e55de7c317b898dee7805d379d3fe
ram16 <thomass.montaine@gmail.com>
2026-09-03 00:32:19 +0200

    Update Util_rigidbodies.cs

--- Runtime/Physics/Util_rigidbodies.cs
+++ Runtime/Physics/Util_rigidbodies.cs
@@ -23,22 +23,29 @@ partial class Util
         return value;
     }
 
-    public static Quaternion SmoothDamp(in Quaternion current, in Quaternion target, ref Vector3 angularVelocity, in float damp, in float maxSpeed, in float deltaTime)
+    public static Quaternion SmoothDampRotation_degrees(in Quaternion current, in Quaternion target, ref Vector3 angularVelocity, in float damp, in float maxSpeed, in float deltaTime)
     {
         Vector3 eulers = (target * Quaternion.Inverse(current)).eulerAngles.SignedEulers();
         Vector3.SmoothDamp(Vector3.zero, eulers, ref angularVelocity, damp, maxSpeed, deltaTime);
         return Quaternion.Euler(deltaTime * angularVelocity) * current;
     }
 
-    public static void SmoothDamp(this Rigidbody rigidbody, in Quaternion target, in float damp, in float maxSpeed, in float control)
+    public static Quaternion SmoothDampRotation_radians(in Quaternion current, in Quaternion target, ref Vector3 angularVelocity, in float damp, in float maxSpeed, in float deltaTime)
+    {
+        Vector3 eulers = (target * Quaternion.Inverse(current)).eulerAngles.SignedEulers() * Mathf.Deg2Rad;
+        Vector3.SmoothDamp(Vector3.zero, eulers, ref angularVelocity, damp, maxSpeed, deltaTime);
+        return Quaternion.Euler(Mathf.Rad2Deg * deltaTime * angularVelocity) * current;
+    }
+
+    public static void SmoothDampRotation_radians(this Rigidbody rigidbody, in Quaternion target, in float damp, in float maxSpeed, in float control)
     {
         Vector3 angularVelocity1 = rigidbody.angularVelocity;
         Vector3 angularVelocity2 = angularVelocity1 * Mathf.Rad2Deg;
-        SmoothDamp(rigidbody.rotation, target, ref angularVelocity2, damp, maxSpeed, Time.fixedDeltaTime);
+        SmoothDampRotation_degrees(rigidbody.rotation, target, ref angularVelocity2, damp, maxSpeed, Time.fixedDeltaTime);
         rigidbody.AddTorque(control * (angularVelocity2 * Mathf.Deg2Rad - angularVelocity1), ForceMode.VelocityChange);
     }
 
-    public static void SmoothDamp(this Rigidbody rigidbody, in Vector3 target, in float damp, in float maxSpeed, in float control)
+    public static void SmoothDampPosition(this Rigidbody rigidbody, in Vector3 target, in float damp, in float maxSpeed, in float control)
     {
         Vector3 velocity = rigidbody.linearVelocity;
         Vector3.SmoothDamp(rigidbody.position, target, ref velocity, damp, maxSpeed, Time.fixedDeltaTime);
